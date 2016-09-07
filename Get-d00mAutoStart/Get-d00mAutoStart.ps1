64e5dc32a2a09554134295dc8967c1fc1936fa17
Alex <george.Sparkman87@gmail.com>
2016-09-06 20:01:19 -0500

    Get-d00mAutoStart
    Initial commit of function. Look into cleaning up output to something
    different from write-outputing a different object for each autostart?
    Also need to write Pester test file.

---
+++ Get-d00mAutoStart/Get-d00mAutoStart.ps1
@@ -0,0 +1,78 @@
+﻿<#
+.SYNOPSIS
+    Get start up programs information
+
+.DESCRIPTION
+    Query Win32_StartupCommand for process name, location, user, command,
+    and description information for local and remote computers
+
+.EXAMPLE
+    Get-d00mAutoStart
+
+    This example gets start up process information for the local computer
+
+.EXAMPLE
+    Get-d00mAutoStart -ComputerName computer1, computer2
+
+    This example gets start up process information for the remote
+    computers, Computer1 and Computer2
+
+.EXAMPLE
+    Get-AdComputer | Get-d00mAutoStart
+
+    This example gets a list of computers from AD and retrieves auto start
+    process information for each
+#>
+function Get-d00mAutoStart
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start = Get-Date
+        Write-Verbose ('{0} : Begin execution : {1}' -f $cmdletName, $start)
+    }
+
+    process
+    {
+        foreach ($computer in $ComputerName)
+        {
+            Write-Verbose ('{0} : {1} : Begin execution' -f $cmdletName, $computer)
+            try
+            {
+                $params = @{ComputerName = $computer
+                            ClassName    = 'Win32_StartupCommand'
+                            ErrorAction  = 'Stop'}
+                $autostarts = Get-CimInstance @params
+                foreach ($autostart in $autostarts)
+                {
+                    $props = @{ComputerName = $computer
+                               Name         = $autostart.Name
+                               Location     = $autostart.Location
+                               User         = $autostart.User
+                               Command      = $autostart.Command
+                               Description  = $autostart.Description}
+                    $obj = New-Object psobject -Property $props
+                    Write-Output $props
+                }
+            }
+            catch
+            {
+                Write-Output $false
+            }
+        }
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
