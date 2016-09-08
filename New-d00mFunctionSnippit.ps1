fd113d77fc11caff41e5cf06d0046366317f918d
Alex <george.Sparkman87@gmail.com>
2016-09-07 20:37:27 -0500

    New-d00mFunctionSnippit
    PowerShell ISE snippit to quickly frame out simple d00m function

---
+++ New-d00mFunctionSnippit.ps1
@@ -0,0 +1,44 @@
+﻿$Text = @'
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+    
+.EXAMPLE
+
+.EXAMPLE
+
+.EXAMPLE
+
+#>
+
+function 
+{
+    [CmdletBinding()]
+    param
+    (
+
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, 
+                                                                 $start)
+    }
+
+    process
+    {
+ 
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
+'@
+New-IseSnippet -Title 'd00m Function' -Description 'Create a new d00m function' -Text $Text -Author 'd00mjaw87'
