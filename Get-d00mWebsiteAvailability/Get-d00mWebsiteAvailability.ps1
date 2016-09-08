ccea3b6d51dd7b0f079df26b85571e9192f6b6f4
Alex <george.Sparkman87@gmail.com>
2016-09-07 19:05:25 -0500

    Get-d00mWebsiteAvailability
    Returns status code and "response time" for input-ed urls. And unit
    tests!

---
+++ Get-d00mWebsiteAvailability/Get-d00mWebsiteAvailability.ps1
@@ -0,0 +1,80 @@
+﻿<#
+.SYNOPSIS
+    Checks website availability
+
+.DESCRIPTION
+    Iterates through passed in $Url's (or uses default values for the parameter) and gets website's
+    status code.
+
+.EXAMPLE
+    Get-d00mWebsiteAvailability
+
+    This example gets the website's response status code and status description for each of the 
+    default $Url values
+
+.EXAMPLE
+    Get-d00mWebsiteAvailability -Url 'http://www.google.com'
+
+    This example gets the provided website's response status code and status description
+#>
+
+
+function Get-d00mWebsiteAvailability
+{
+    [cmdletbinding()]
+    param
+    (
+        #Url of the website to check availability
+        [parameter()]
+        [string[]]$Url = @('https://secure.frontstrat.com', 
+                           'https://login.teamfrontera.com', 
+                           'http://www.teamfrontera.com')
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, $start)
+    }
+
+    process
+    {
+        foreach ($website in $Url)
+        {
+            Write-Verbose -Message ('{0} : Invoking {1}' -f $cmdletName, $website)
+            try
+            {
+                $responseTime = (Measure-Command -Expression {
+                    $response = Invoke-WebRequest -Uri $website -UseBasicParsing
+                }).TotalMilliseconds
+                $props = @{Url         = $website
+                           Status      = $response.StatusCode
+                           Description = $response.StatusDescription}
+                if ($response.StatusCode -eq 200)
+                {
+                    $props.Add('Alive', $true)
+                    $props.Add('ResponseTime', $responseTime)
+                }
+                else
+                {
+                    $props.Add('Alive', $false)
+                    $props.Add('ResponseTime', $null)
+                }
+                $obj = New-Object psobject -Property $props
+                Write-Output $obj
+            }
+            catch
+            {
+                throw $Error[0]
+            }
+        }   
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
---
+++ Get-d00mWebsiteAvailability/Get-d00mWebsiteAvailability.tests.ps1
@@ -0,0 +1,34 @@
+﻿$here = Split-Path -Path $MyInvocation.MyCommand.Path
+$file = (Get-ChildItem -Path $here -Filter '*.ps1') | Where-Object -Property Name -NotLike '*.Tests.ps1'
+. $($file.FullName)
+
+Describe 'Get-d00mWebsiteAvailability' {
+    It 'ValidWebsite.Description = OK' {
+        $obj = Get-d00mWebsiteAvailability -Url 'http://www.google.com' 
+        $obj.Description | Should be 'OK'
+    }
+
+    It 'ValidWebsite.Alive = $true' {
+        $obj = Get-d00mWebsiteAvailability -url 'https://portal.office.com'
+        $obj.Alive | Should be $true
+    }
+
+    It 'ValidWebsite.Status = 200' {
+        $obj = Get-d00mWebsiteAvailability -Url 'https://apps.rackspace.com'
+        $obj.Status | Should be '200'
+    }
+
+    It 'ValidWebsite.Status.GetType = Int' {
+        $obj = Get-d00mWebsiteAvailability -Url 'https://apps.rackspace.com'
+        $obj.Status | Should BeOfType [Int]
+    }
+
+    It 'ValidWebsite.ResponseTime > 0' {
+        $obj = Get-d00mWebsiteAvailability -Url 'https://github.com'
+        $obj.ResponseTime | Should BeGreaterThan 0 
+    }
+
+    It 'InvalidWebsite should fail' {
+        {Get-d00mWebsiteAvailability -Url 'failure'} | Should throw
+    }
+}
