1272a50ed6ea878eed4f833ee3fd2473f1529aca
Alex <george.Sparkman87@gmail.com>
2016-09-09 13:44:10 -0500

    New-d00mCredentialFile
    Creates credential file for use with scripts local to the executing
    machine

---
+++ New-d00mCredentialFile/New-d00mCredentialFile.ps1
@@ -0,0 +1,11 @@
+﻿$credsPath = 'c:\credentials.xml'
+if (Test-Path -Path $credsPath)
+{
+    Write-Warning -Message ('{0} already exists, creating backup version' -f $credsPath)
+    Move-Item -Path $credsPath -Destination ('c:\credentials_{0}.backup' -f (Get-Date -Format 'yyyyMMdd')) -Force
+}
+$credentials = @{'DomainAdmin' = (Get-Credential -Message 'Domain administrator')
+                 'DomainUser'  = (Get-Credential -Message 'Domain user')
+                 'O365Admin'   = (Get-Credential -Message 'Office 365 administrator')
+                 'O365User'    = (Get-Credential -Message 'Office 365 user')}
+$credentials | Export-Clixml -Path $credsPath -Force
