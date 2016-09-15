76ee9bf9155b48d2a7d9c072abff21027e881202
Alex <george.Sparkman87@gmail.com>
2016-09-15 15:57:59 -0500

    Get-d00mLastBootupTime
    Function to return Win32_ComputerSystem's lastbootuptime property.
    Pester tests included!

---
+++ Get-d00mLastBootupTime/Get-d00mLastBootupTime.Tests.ps1
@@ -0,0 +1,33 @@
+﻿$here = Split-Path -Path $MyInvocation.MyCommand.Path
+$file = (Get-ChildItem -Path $here -Filter '*.ps1') | Where-Object -Property Name -NotLike '*.Tests.ps1'
+. $($file.FullName)
+
+Describe 'Get-d00mLastBootupTime' {
+    It 'Does not throw exception on local computer' {
+        {Get-d00mLastBootupTime} | 
+            Should not throw
+    }
+
+    It 'Should throw on invalid computer' {
+        {Get-d00mLastBootupTime -ComputerName 'Failure'} | 
+            Should throw
+    }
+
+    It 'Outputs String for computername' {
+        $result = Get-d00mLastBootupTime
+        $result.ComputerName |
+            Should BeOftype [System.String]
+    }
+
+    It 'Outputs DateTime for LastBootUpTime' {
+        $result = Get-d00mLastBootupTime
+        $result.LastBootupTime | 
+            Should BeOfType [System.DateTime]
+    }
+
+    It 'Outputs TimeSpan for Difference' {
+        $result = Get-d00mLastBootupTime
+        $result.Difference | 
+            Should BeOfType [System.TimeSpan]
+    }
+}
---
+++ Get-d00mLastBootupTime/Get-d00mLastBootupTime.ps1
@@ -0,0 +1,88 @@
+﻿<#
+.SYNOPSIS
+    Get last boot up time from computer(s)
+
+.DESCRIPTION
+    Query computer(s) Win32_OperatingSystem for LastBootUpTime
+
+.EXAMPLE
+    Get-d00mLastBootupTime
+
+    This example returns the last boot up time for the local machine
+
+.EXAMPLE
+    Get-d00mLastBootupTime -ComputerName Computer1, Computer2 -Credential (Get-Credential)
+
+    This example returns the last boot up time for the identified remote computers using
+    the supplied credentials
+
+.EXAMPLE
+    Read-Content -Path c:\list.txt | Get-d00mLastBootupTime
+
+    This example feeds a list of computers from the file to the function to query Win32_Computersystem
+    to get the last boot up time
+#>
+
+function Get-d00mLastBootupTime
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [parameter()]
+        [pscredential]$Credential
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, 
+                                                                 $start)
+    }
+
+    process
+    {
+        foreach ($computer in $ComputerName)
+        {
+            try
+            {
+                $sessionParams = @{ComputerName = $computer
+                                   ErrorAction  = 'Stop'}
+                if ($Credential -ne $null)
+                {
+                    $sessionParams.Add('Credential', $Credential)
+                }
+                $session = New-CimSession @sessionParams
+
+                $cimParams = @{CimSession  = $session
+                               ClassName   = 'Win32_OperatingSystem'
+                               Property    = 'LastBootupTime'
+                               ErrorAction = 'Stop'}
+                $lastBoot  = Get-CimInstance @cimParams | 
+                    Select-Object -ExpandProperty LastBootupTime
+                $difference = $(Get-Date) - $lastBoot
+                New-Object -TypeName psobject -Property $([ordered]@{ComputerName   = $computer
+                                                                     LastBootupTime = $lastBoot
+                                                                     Difference     = $difference}) |
+                    Write-Output
+                Remove-CimSession -CimSession $session
+            }
+
+            catch
+            {
+                throw
+            }
+        }
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
