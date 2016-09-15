0a299b7682c8d6c3a580599bc8858735cca13397
Alex <george.Sparkman87@gmail.com>
2016-09-15 09:41:37 -0500

    Get-d00mCpuLoad
    Complete! With Pester unit tests!

--- Get-d00mCpuLoad/Get-d00mCpuLoad.ps1
+++ Get-d00mCpuLoad/Get-d00mCpuLoad.ps1
@@ -19,10 +19,11 @@
     from the remote computers- Computer1, Computer2, Computer3
 
 .EXAMPLE
-    Get-Content C:\list.txt | Get-d00mCpuLoad
+    Get-Content C:\list.txt | Get-d00mCpuLoad -Credential (Get-Credential)
 
     This example returns the average CIM class Win32_Processor LoadPercentage
-    from the list of computer names in the file c:\list.txt
+    from the list of computer names in the file c:\list.txt with the supplied
+    credential.
 
 .EXAMPLE
     Get-AdComputer -Filter {(Enabled -eq 'true')} | Get-d00mCpuLoad
@@ -39,7 +40,10 @@ function Get-d00mCpuLoad
         # Name(s) of computers to query
         [parameter(ValueFromPipeline=$true,
                    ValueFromPipelineByPropertyName=$true)]
-        [string[]]$ComputerName = $env:COMPUTERNAME
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [parameter()]
+        [pscredential]$Credential
     )
 
     begin
@@ -55,19 +59,35 @@ function Get-d00mCpuLoad
         foreach ($computer in $ComputerName)
         {
             Write-Verbose -Message ('{0} : {1} : Begin execution' -f $cmdletName, $computer)
-            $average = try
+
+            try
             {
-                Get-CimInstance -ClassName Win32_Processor -ComputerName $computer -ErrorAction Stop |
+                $params = @{ClassName    = 'Win32_Processor'
+                            ComputerName = $computer
+                            ErrorAction  = 'Stop'}
+                if ($Credential -ne $null)
+                {
+                    $params.Add('Credential', $Credential)
+                    Write-Verbose ('{0} : Accessing {1} using $Credential parameter' -f $cmdletName, $computer)
+                }
+                else
+                {
+                    Write-Verbose ('{0} : Accessing {1} using default credentials' -f $cmdletName, $computer)
+                }
+
+                $average = Get-CimInstance @params |
                     Measure-Object -Property LoadPercentage -Average |
                     Select-Object -ExpandProperty Average
+
+                New-Object -TypeName psobject -Property @{ComputerName = $computer
+                                                          CPULoad      = $average} |
+                    Write-Output
             }
             catch
             {
-                'Error reaching {0}' -f $computer
+                throw
             }
-            New-Object -TypeName psobject -Property @{ComputerName = $computer
-                                                      CPULoad      = $average} |
-                Write-Output
+
         }
     }
 
---
+++ Get-d00mCpuLoad/Get-d00mCpuLoad.tests.ps1
@@ -0,0 +1,48 @@
+﻿$here = Split-Path -Path $MyInvocation.MyCommand.Path
+$file = (Get-ChildItem -Path $here -Filter '*.ps1') | Where-Object -Property Name -NotLike '*.Tests.ps1'
+. $($file.FullName)
+
+Describe 'Get-d00mCpuLoad' {
+    It 'Throws exception for invalid computername' {
+        {Get-d00mCpuLoad -ComputerName 'Failure'} | 
+            Should throw
+    }
+
+    It 'Outputs a double for cpu load' {
+        $result = Get-d00mCpuLoad
+        $result.CPULoad | 
+            Should BeOfType System.Double
+    }
+
+    It 'Outputs a string for computername' {
+        $result = Get-d00mCpuLoad
+        $result.ComputerName | 
+            Should BeOfType System.String
+    }
+
+    It 'Accepts pipeline input for computername' {
+        {'Fs-Test' | Get-d00mCpuLoad} | 
+            Should Not throw
+    }
+
+    It 'Accepts multiple pipeline input for computername' {
+        {'Clinicalops1', 'Clinicalops2', 'Clinicalops3', 'Clinicalops4' | Get-d00mCpuLoad} |
+            Should Not Throw
+    }
+
+    It 'Accepts computernames feed from file' {
+        $list = New-Object -TypeName System.Text.StringBuilder
+        $list.AppendLine('fs-test') | Out-Null
+        $list.AppendLine('fs-it-admin') | Out-Null
+        New-Item -Path $here -Name 'names.txt' -ItemType File -Value $list.ToString() -Force
+        {Get-Content -Path $here\names.txt | Get-d00mCpuLoad} |
+            Should Not Throw
+        Remove-Item -Path $here\names.txt -Force
+    }
+
+    It 'Throws exception for invalid credentials' {
+        $credential = New-Object -TypeName pscredential -ArgumentList 'notreal', ('failure' | ConvertTo-SecureString -AsPlainText -Force)
+        {Get-d00mCpuLoad -ComputerName -Credential ($credential)} | 
+            Should throw
+    }
+}
