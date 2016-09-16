093cc857996b9568a14f854e06ca0daaa48324bd
Alex <george.Sparkman87@gmail.com>
2016-09-15 20:51:24 -0500

    d00m module
    d00m Module!

---
+++ Modules/d00m/d00m.psm1
@@ -0,0 +1,97 @@
+﻿function Connect-Frontera
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter()]
+        [string]$ConnectionName = 'FS-VPN',
+
+        [parameter()]
+        [string]$ComputerName   = 'fs-it-admin.frontera.msft'
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, 
+                                                                 $start)
+    }
+
+    process
+    {
+        if (!(Get-NetIPInterface -InterfaceAlias $ConnectionName -ErrorAction SilentlyContinue))
+        {
+            $params = @{FilePath     = 'rasdial.exe'
+                        NoNewWindow  = $true
+                        ArgumentList = $ConnectionName
+                        ErrorAction  = 'Stop'
+                        Wait         = $true}
+            Start-Process @params
+            Write-Verbose -Message ('{0} : Connected to VPN : {1}' -f $cmdletName,
+                                                                      $ConnectionName)
+        }
+        else
+        {
+            Write-Warning -Message ('Already connected to {0}' -f $ConnectionName)
+        }
+
+        Write-Verbose -Message ('{0} : Connecting RDP : {1}' -f $cmdletName,
+                                                                $ComputerName)
+        $params = @{ComputerName = $ComputerName
+                    Count        = 1
+                    ErrorAction  = 'SilentlyContinue'}
+        While (!(Test-Connection @params))
+        {
+            Write-Verbose -Message ('{0} : Could not find {1}... waiting...' -f $cmdletName,
+                                                                                $ComputerName)
+            Start-Sleep -Seconds 1
+        }
+        Start-Process -FilePath mstsc.exe -ArgumentList "/v $ComputerName"
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
+
+function Disconnect-Frontera
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter()]
+        [string]$ConnectionName = 'FS-VPN'
+    )
+
+    begin
+    {
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, 
+                                                                 $start)
+    }
+
+    process
+    {
+        if (Get-NetIPInterface -InterfaceAlias $ConnectionName -ErrorAction SilentlyContinue)
+        {
+            $params = @{FilePath     = 'rasdial.exe'
+                        NoNewWindow  = $true
+                        ArgumentList = '/d'
+                        ErrorAction  = 'SilentlyContinue'
+                        Wait         = $true}
+            Start-Process @params
+        }
+    }
+
+    end
+    {
+        $end = ($(Get-Date) - $start).TotalMilliseconds
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $end)
+    }
+}
