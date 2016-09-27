d0a649b2aead1f4b13ad2073bd669921331ee5e5
Alex <george.Sparkman87@gmail.com>
2016-09-27 15:34:40 -0500

    d00m AD Functions
    Get-d00mAdRsop for getting Resultant Set of Policy for AD Computers and
    Users. Get-d00mAdUserReport WIP, just need to pick out what AD
    attributes to stick in a fancy looking table

---
+++ Get-d00mAdRsop/Get-d00mAdRsop.ps1
@@ -0,0 +1,169 @@
+﻿#requires -Modules ActiveDirectory
+
+<#
+.SYNOPSIS
+    Generates GPResult for specified computers in domain
+
+.DESCRIPTION
+    Generates GPResult reports for...
+
+    1) Specified computers
+    2) All non-server enabled Windows computers
+    3) All server enabled Windows computers
+    4) All enabled Windows computers
+
+    And saves HTML report in specified directory, or current directory by default
+
+.EXAMPLE
+    Get-d00mAdRsop -ComputerName Computer1, Computer1
+
+    This example generates a GPResult HTML report for the specified computers,
+    Computer1 and Computer2, for the user set in the ManagedBy AD property
+    and saves the HTML report in the current directory
+
+.EXAMPLE
+    Get-d00mAdRsop -Servers -FilePath C:\Path
+
+    This example generates a GPResult HTML report for all enabled domain computers with
+    *Server* part of the OperatingSystem AD attribute and saves the report in c:\Path
+
+.EXAMPLE
+    Get-d00mAdRsop -Computers
+
+    This example generates a GPResult HTML report for all enabled domain computers without
+    *Server* part of the OperatingSystem AD attribute and saves the reports in the current
+    directory
+#>
+function Get-d00mAdRsop
+{
+    [CmdletBinding(DefaultParameterSetName='ComputerName')]
+    param
+    (
+        [parameter(ParameterSetName='ComputerName',
+                   ValueFromPipelineByPropertyName)]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [parameter(ParameterSetName='Computers')]
+        [switch]$Computers,
+
+        [parameter(ParameterSetName='Servers')]
+        [switch]$Servers,
+
+        [parameter(ParameterSetName='Everything')]
+        [switch]$Everything,
+
+        [parameter()]
+        [string]$FilePath = (Get-Location)
+    )
+
+    begin
+    {
+        $timer = New-Object -TypeName System.Diagnostics.StopWatch
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, (Get-Date))
+        $timer.Start()
+    }
+
+    process
+    {
+        Write-Verbose -Message ('{0} : ParameterSetName : {1}' -f $cmdletName, $PSCmdlet.ParameterSetName)
+        $adComputers = Switch ($PSCmdlet.ParameterSetName)
+        {
+            'ComputerName'
+            {
+                foreach ($computer in $ComputerName)
+                {
+                    Write-Verbose -Message ('{0} : Getting {1} AD object' -f $cmdletName, $computer)
+                    try
+                    {
+                        $params = @{Properties  = 'ManagedBy'
+                                    Identity    = $computer
+                                    ErrorAction = 'Stop'}
+                        Get-AdComputer @params
+                    }
+                    catch
+                    {
+                        throw
+                    }
+                }
+            }
+
+            'Computers'
+            {
+                Write-Verbose -Message ('{0} : Getting all computer AD objects' -f $cmdletName)
+                try
+                {
+                    $params = @{Properties =  'ManagedBy'
+                                Filter     =  {(enabled -eq 'true') -and (operatingsystem -notlike '*server*') -and (operatingsystem -like '*windows*')}
+                                ErrorAction = 'Stop'}
+                    Get-ADComputer @params
+                }
+                catch
+                {
+                    throw
+                }
+            }
+
+            'Servers'
+            {
+                Write-Verbose -Message ('{0} : Getting all server AD objects' -f $cmdletName)
+                try
+                {
+                    $params = @{Properties =  'ManagedBy'
+                                Filter     =  {(enabled -eq 'true') -and (operatingsystem -like '*server*') -and (operatingsystem -like '*windows*')}
+                                ErrorAction = 'Stop'}
+                    Get-ADComputer @params
+                }
+                catch
+                {
+                    throw
+                }
+            }
+
+            'Everything'
+            {
+                Write-Verbose -Message ('{0} : Getting all AD objects' -f $cmdletName)
+                try
+                {
+                    $params = @{Properties =  'ManagedBy'
+                                Filter     =  {(enabled -eq 'true') -and (operatingsystem -like '*windows*')}
+                                ErrorAction = 'Stop'}
+                    Get-ADComputer @params
+                }
+                catch
+                {
+                    throw
+                }
+            }
+        }
+
+        Write-Verbose -Message ('{0} : Begin execution on {1} AD objects' -f $cmdletName, $adComputers.Count)
+        foreach ($computer in $adComputers)
+        {
+            try
+            {
+                Write-Verbose -Message ('{0} : {1} : Begin execution' -f $cmdletName, $computer.Name)
+
+                $adUser = Get-ADUser -Identity ($computer.ManagedBy)
+                Write-Verbose -Message ('{0} : {1} : ManagedBy : {2}' -f $cmdletName, $computer.Name, $adUser.SamAccountName)
+
+                $reportFileName = '{0}_{1}_{2}-gpresult.html' -f $computer.Name, $adUser.SamAccountName, (Get-date -Format 'yyyyMMdd')
+                $reportFilePath = Join-Path -Path $FilePath -ChildPath $reportFileName
+                
+                $argument = '/s {0} /User {1} /H {2} /F' -f $computer.Name, $adUser.SamAccountName, $reportFilePath
+                Start-Process -FilePath gpresult.exe -ArgumentList $argument -NoNewWindow
+            }
+            catch
+            {
+                throw
+            }
+        }
+    }
+
+    end
+    {
+        $timer.Stop()
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $timer.Elapsed.TotalMilliseconds)
+    }
+}
---
+++ Get-d00mAdUserReport/Get-d00mAdUserReport.ps1
@@ -0,0 +1,160 @@
+﻿#requires -Modules ActiveDirectory
+
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+    
+.EXAMPLE
+
+.EXAMPLE
+
+.EXAMPLE
+
+#>
+function Get-d00mAdUserReport
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [String]$FilePath = (Get-Location)
+    )
+
+    begin
+    {
+        $timer = New-Object -TypeName System.Diagnostics.StopWatch
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, (Get-Date))
+        $timer.Start()
+    }
+
+    process
+    {
+        Write-Verbose -Message ('{0} : Report directory : {1}' -f $cmdletName, $FilePath)
+        
+        try
+        {
+            $adUsers = Get-AdUser -Filter * -Properties * -ErrorAction Stop
+            Write-Verbose -Message ('{0} : Found {1} AD user objects' -f $cmdletName, $adUsers.Count)
+
+            if ($adUsers.Count -gt 0)
+            {
+                $html = New-Object -TypeName System.Text.StringBuilder
+                $html.AppendLine("
+                <html>
+                    <head>
+                        <title>d00m AD User Report</title>
+                        <style>
+                            table, tr, td {
+                                border: 1px solid green;
+                                border-collapse: collapse;
+                            }
+                            
+                            tr.alt td {
+                                background-color: `#171717;
+                            }
+
+                            tr.heading td {
+                                font-weight: bold;
+                                text-align: center;
+                                font-size: larger;
+                                color: white;
+                                background-color: `#333333;
+                            }
+
+                            body {
+                                background-color: black;
+                                color: `#bdbdbd;
+                                font-family: lucida consolas, monospace;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <table>
+                            <tr class=`"heading`">
+                                <td colspan=`"2`">d00m AD User Report</td>
+                            </tr>
+                            <tr>
+                                <td>Report</td>
+                                <td>$($cmdletName)</td>
+                            </tr>
+                            <tr>
+                                <td>Date</td>
+                                <td>$(Get-Date)</td>
+                            </tr>
+                        </table>
+                        </br>
+                        <table>
+                            <tr class=`"heading`">
+                                <td>CannotChangePassword</td>
+                                <td>Enabled</td>
+                                <td>LastBadPasswordAttempt</td>
+                                <td>Company</td>
+                                <td>Description</td>
+                                <td>DisplayName</td>
+                                <td>PasswordLastSet</td>
+                                <td>Department</td>
+                                <td>PasswordNeverExpires</td>
+                                <td>WhenChanged</td>
+                                <td>ObjectGuid</td>
+                                <td>WhenCreated</td>
+                                <td>EmailAddress</td>
+                                <td>PasswordExpired</td>
+                                <td>ObjectSID</td>
+                                <td>CanicalName</td>
+                                <td>LastLogonDate</td>
+                            </tr>") | Out-Null
+
+                $count = 1
+                $adUsers = $adUsers | Sort-Object -Property SurName
+                foreach ($adUser in $adUsers)
+                {
+                    if ([bool]!($count%2))
+                    {
+                        $html.AppendLine('<tr class="alt">') | Out-Null
+                    }
+                    else
+                    {
+                        $html.AppendLine('<tr>') | Out-Null
+                    }
+
+                    $html.AppendLine("
+                                <td>$($adUser.CannotChangePassword)</td>
+                                <td>$($adUser.Enabled)</td>
+                                <td>$($adUser.LastBadPasswordAttempt)</td>
+                                <td>$($adUser.Company)</td>
+                                <td>$($adUser.Description)</td>
+                                <td>$($adUser.DisplayName)</td>
+                                <td>$($adUser.PasswordLastSet)</td>
+                                <td>$($adUser.Department)</td>
+                                <td>$($adUser.PasswordNeverExpires)</td>
+                                <td>$($adUser.WhenChanged)</td>
+                                <td>$($adUser.ObjectGuid)</td>
+                                <td>$($adUser.WhenCreated)</td>
+                                <td>$($adUser.EmailAddress)</td>
+                                <td>$($adUser.PasswordExpired)</td>
+                                <td>$($adUser.ObjectSID)</td>
+                                <td>$($adUser.CanicalName)</td>
+                                <td>$($adUser.LastLogonDate)</td>
+                            </tr>") | Out-Null
+                    $count++
+                }
+                $fileName = 'ADUserReport_{0}.html' -f (Get-Date -Format 'yyyyMMdd')
+                $FullPath = Join-Path -Path $FilePath -ChildPath $fileName
+                $html.ToString() | Out-File -FilePath $FullPath
+            }
+        }
+        catch
+        {
+            throw
+        }
+    }
+
+    end
+    {
+        $timer.Stop()
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $timer.Elapsed.TotalMilliseconds)
+    }
+}
