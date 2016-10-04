2e8335e22eb77beaeecb7439c3a2feecb40746d3
Alex <george.Sparkman87@gmail.com>
2016-10-03 20:39:23 -0500

    Jibberish!
    Create jibberish! Be silly!

---
+++ New-d00mJibberish.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module C:\github\P0w3rSh3ll\Modules\d00m\d00m.psm1 -Force
+$alphabet = @('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+
+$counter = 1
+$thingToSay = New-Object -TypeName System.Text.StringBuilder
+while ($counter -le 1000)
+{
+    $thingToSay.Append($($alphabet | Get-Random)) | Out-Null 
+    $counter++
+}
+$t = Get-d00mSayThings $thingToSay.ToString()
+$t.Spoke
