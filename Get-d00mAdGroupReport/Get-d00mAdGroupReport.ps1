b0ac9a8f527abece0f93bf1e02c0f8b38329092d
Alex <george.Sparkman87@gmail.com>
2016-10-07 15:34:01 -0500

    Ad Group Report and BackupStatus Reports!
    Completed function scripts, maybe add to d00mReport later?

---
+++ Get-d00mAdGroupReport/Get-d00mAdGroupReport.ps1
@@ -0,0 +1,196 @@
+﻿#requires -modules ActiveDirectory
+
+<#
+.SYNOPSIS
+    Active Directory group HTML report
+
+.DESCRIPTION
+    Generate HTML report on Active Directory group's properties and members
+
+.EXAMPLE
+    Get-d00mAdGroupReport
+
+    This example will generate a HTML report on all Active Directory groups
+    properites and members saved to the current directory
+
+.EXAMPLE
+    Get-d00mAdGroupReport -GroupName Group1, Group2 -FilePath \\server1\share
+
+    This example will generate a HTML report on the Group1 and Group2
+    Active Directory groups properties and members saved to the specified
+    directory
+#>
+
+function Get-d00mAdGroupReport
+{
+    [CmdletBinding()]
+    param
+    (
+        [Alias('Name')]
+        [parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string[]]$GroupName,
+
+        [parameter()]
+        [string]$FilePath
+    )
+
+    begin
+    {
+        $timer = New-Object -TypeName System.Diagnostics.StopWatch
+        $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
+        Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, (Get-Date))
+        $timer.Start()
+    }
+
+    process
+    {
+        try
+        {
+            $html = New-Object -TypeName System.Text.StringBuilder
+            $html.AppendLine("
+            <html>
+                <head>
+                    <title>d00m AD Group Report</title>
+                    <style>
+                        table, tr, td {
+                            border: 1px solid green;
+                            border-collapse: collapse;
+                        }
+                            
+                        tr.alt td {
+                            background-color: `#171717;
+                        }
+
+                        tr.heading td {
+                            font-weight: bold;
+                            text-align: center;
+                            font-size: larger;
+                            color: white;
+                            background-color: `#333333;
+                        }
+
+                        body {
+                            background-color: black;
+                            color: `#bdbdbd;
+                            font-family: lucida consolas, monospace;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <table>
+                        <tr class=`"heading`">
+                            <td colspan=`"2`">d00m AD Group Report</td>
+                        </tr>
+                        <tr>
+                            <td>Report</td>
+                            <td>$($cmdletName)</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td>$(Get-Date)</td>
+                        </tr>
+                    </table>
+                    </br>") | Out-Null
+
+            if ($GroupName -eq $null)
+            {
+                Write-Verbose -Message ('{0} : GroupName is null. Getting all AD groups' -f $cmdletName)
+                $GroupName = (Get-ADGroup -Filter *).Name
+            }
+        
+            if ($FilePath -eq '')
+            {
+                $FilePath = (Get-Location)
+                Write-Verbose -Message ('{0} : FilePath is null. Saving report to current directory' -f $cmdletName)
+            }
+
+            $GroupName = $GroupName | Sort-Object
+
+            foreach ($group in $GroupName)
+            {
+                Write-Verbose -Message ('{0} : {1} : Begin execution' -f $cmdletName, $group)
+
+                Write-Verbose -Message ('{0} : {1} : Getting properties' -f $cmdletName, $group)
+                $adGroup = Get-AdGroup -Identity $group -Properties *
+                $html.AppendLine(('
+                    <table>
+                        <tr class="heading">
+                            <center>
+                                <td colspan="2">{0}</td>
+                            </center>
+                        </tr>
+                        <tr>
+                            <td>DistinguishedName</td>
+                            <td>{1}</td>
+                        </tr>
+                        <tr>
+                            <td>GroupCategory</td>
+                            <td>{2}</td>
+                        </tr>
+                        <tr>
+                            <td>GroupScope</td>
+                            <td>{3}</td>
+                        </tr>
+                        <tr>
+                            <td>SID</td>
+                            <td>{4}</td>
+                        </tr>
+                        <tr>
+                            <td>Description</td>
+                            <td>{5}</td>
+                        </tr>
+                        <tr>
+                            <td>WhenCreated</td>
+                            <td>{6}</td>
+                        </tr>
+                        <tr>
+                            <td>WhenChanged</td>
+                            <td>{7}</td>
+                        </tr>
+                        <tr class="heading">
+                            <center>
+                                <td colspan="2">Members</td>
+                            </center>
+                        </tr>' -f $adGroup.Name, 
+                                  $adGroup.DistinguishedName, 
+                                  $adGroup.GroupCategory, 
+                                  $adGroup.GroupScope, 
+                                  $adGroup.SID, 
+                                  $adGroup.Description,
+                                  $adGroup.WhenCreated,
+                                  $adGroup.WhenChanged)) | Out-Null
+            
+                Write-Verbose -Message ('{0} : {1} : Getting members' -f $cmdletName, $group)
+                $members = Get-ADGroupMember -Identity $adGroup
+                foreach ($member in $members)
+                {
+                    $html.AppendLine(('
+                        <tr>
+                            <td colspan="2">{0}</td>
+                        </tr>' -f $member.DistinguishedName)) | Out-Null
+                }
+                $html.AppendLine('
+                    </table>
+                    </br>') | Out-Null
+
+                Write-Verbose -Message ('{0} : {1} : End execution' -f $cmdletName, $group)
+            }
+
+            $reportName = 'ADGroupReport_{0}.html' -f (Get-Date -Format 'yyyyMMdd')
+            $html.ToString() | Out-File -FilePath (Join-Path -Path $FilePath -ChildPath $reportName) -Force
+        }
+        catch
+        {
+            throw
+        }
+    }
+
+    end
+    {
+        $timer.Stop()
+        Write-Verbose -Message ('{0} : End execution' -f $cmdletName)
+        Write-Verbose -Message ('Total execution time: {0} ms' -f $timer.Elapsed.TotalMilliseconds)
+    }
+}
--- Get-d00mBackupStatus/Get-d00mBackupStatus.ps1
+++ Get-d00mBackupStatus/Get-d00mBackupStatus.ps1
@@ -1,28 +1,31 @@
 ﻿<#
 .SYNOPSIS
-    Get backup status from computer
+    Get server backup status
 
 .DESCRIPTION
-    Queries Microsoft-Windows-Backup event log for event ID 4 and where Provider
-    name is Microsoft-Windows-Backup and returns newest entry
+    Gets backup policy and summary from servers and creates HTML report
 
 .EXAMPLE
-    Get-d00mBackupStatus
+    Get-d00mBackupStatus -ComputerName Server1 -OperatingSystem 2012
 
-    This example gets the latest backup datetime from local machine using default
-    credentials
-
-.EXAMPLE
-    Get-d00mBackupStatus -ComputerName Server1, Server2 -Credential (Get-Credential)
-
-    This example gets the latest backup datetime from the remote computers, Server1
-    and Server2, using supplied credentials
+    This example retrieves the backup policy and summary from Server1 using
+    the Server 2012 WindowsServerBackup PowerShell module and creates an HTML
+    report in the current directory
 
 .EXAMPLE
-    (Get-AdComputer).Name | Get-d00mBackupStatus -Credential (Get-Credential)
+    Get-d00mBackupStatus -ComputerName Server1, Server2 -OperatingSystem 2012 -FilePath \\server1\share
 
-    This example gets the latest backup datetime from the computer names returned
-    from the Get-AdComputer cmdlet using supplied credentials
+    This example retrieves the backup policy and summary from Server1, Server2 using the
+    Server 2012 WindowsServerBakcup PowerShell module and creates an HTML report
+    in the specified file path
+
+.EXAMPLE
+    Get-ADComputer -Filter {(enabled -eq 'true') -and (operatingsystem -like '*server*') -and (location -notlike '*azure*')} -Properties name, operatingsystem | 
+        select name, operatingsystem | 
+        Get-d00mBackupStatus -Verbose -FilePath '\\server1\share\'
+
+    This example retrieves the backup policy and summary from all AD servers using either
+    the PSSnapin or module to create an HTML report in the specified file path
 
 #>
 function Get-d00mBackupStatus
@@ -30,11 +33,17 @@ function Get-d00mBackupStatus
     [CmdletBinding()]
     param
     (
-        #Computer name(s) to check
-        [parameter(ValueFromPipeline,
-                   ValueFromPipelineByPropertyName,
-                   Position=0)]
-        [string[]]$ComputerName = $env:COMPUTERNAME,
+        [alias('Name')]
+        [parameter(mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string[]]$ComputerName,
+
+        [parameter(mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string[]]$OperatingSystem,
+
+        [parameter()]
+        [string]$FilePath,
 
         [parameter()]
         [pscredential]$Credential
@@ -44,46 +53,155 @@ function Get-d00mBackupStatus
     {
         $timer = New-Object -TypeName System.Diagnostics.StopWatch
         $cmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+        $start      = Get-Date
         Write-Verbose -Message ('{0} : Begin execution : {1}' -f $cmdletName, (Get-Date))
         $timer.Start()
     }
 
     process
     {
-        foreach ($computer in $ComputerName)
+        try
         {
-            $filter = @{LogName      = 'Microsoft-Windows-Backup'
-                        ProviderName = 'Microsoft-Windows-Backup'
-                        Id           = 4}
-        
-            $params = @{FilterHashTable = $filter
-                        ErrorAction     = 'Stop'
-                        ComputerName    = $computer}
-            
-            if ($Credential -ne $null)
+            foreach ($computer in $ComputerName)
             {
-                Write-Verbose -Message ('{0} : Using supplied credentials' -f $cmdletName)
-                $params.Add('Credential', $Credential) | Out-Null
-            }
-            else
-            {
-                Write-Verbose -Message ('{0} : Using default credentials' -f $cmdletName)
-            }
+                Write-Verbose -Message ('{0} : {1} : Begin execution' -f $cmdletName, $computer)
 
-            try
-            {
-                Write-Verbose -Message ('{0} : {1} : Getting event log' -f $cmdletName, $computer)
-                $event = Get-WinEvent @params |
-                    Select-Object -First 1
-                New-Object -TypeName psobject -Property @{ComputerName = $computer
-                                                          TimeCreated  = $event.TimeCreated
-                                                          Message      = $event.Message} | 
-                    Write-Output
+                $params = @{ComputerName = $computer
+                            ErrorAction  = 'Stop'}
+
+                if ($Credential -ne $Null)
+                {
+                    $params.Add('Credential', $Credential)
+                    Write-Verbose -Message ('{0} : {1} : Using supplied credentials' -f $cmdletName, $computer)
+                }
+                else
+                {
+                    Write-Verbose -Message ('{0} : {1} : Using default credentials' -f $cmdletName, $computer)
+                }
+
+                if ($OperatingSystem -like '*2008*')
+                {
+                    $params.Add('ScriptBlock', {Add-PSSnapin -Name Windows.ServerBackup
+                                                $summary = Get-WBSummary
+                                                $policy = Get-WBPolicy
+                                                New-Object -TypeName psobject -Property @{ComputerName = $env:COMPUTERNAME
+                                                LastSuccessfulBackupTime = $summary.LastSuccessfulBackupTime
+                                                LastSuccessfulBackupTargetPath = $summary.LastSuccessfulBackupTargetPath
+                                                NextBackupTime = $summary.NextBackupTime
+                                                VolumesToBackup = $policy.VolumesToBackup
+                                                BareMetalRecovery = $policy.BMR
+                                                SystemState = $policy.SystemState
+                                                VssBackupOptions = $policy.VssBackupOptions} | Write-Output})
+                }
+
+                if ($OperatingSystem -like '*2012*')
+                {
+                    $params.Add('ScriptBlock', {Import-Module -Name WindowsServerBackup
+                                                $summary = Get-WBSummary
+                                                $policy = Get-WBPolicy
+                                                New-Object -TypeName psobject -Property @{ComputerName = $env:COMPUTERNAME
+                                                LastSuccessfulBackupTime = $summary.LastSuccessfulBackupTime
+                                                LastSuccessfulBackupTargetPath = $summary.LastSuccessfulBackupTargetPath
+                                                NextBackupTime = $summary.NextBackupTime
+                                                VolumesToBackup = $policy.VolumesToBackup
+                                                BareMetalRecovery = $policy.BMR
+                                                SystemState = $policy.SystemState
+                                                VssBackupOptions = $policy.VssBackupOptions} | Write-Output})
+                }
+
+                Write-Verbose -Message ('{0} : {1} : Getting Backup status' -f $cmdletName, $computer)
+                $result = Invoke-Command @params
+                Write-Verbose -Message ('{0} : {1} : Getting backup status complete' -f $cmdletName, $computer)
+
+                $html = New-Object -TypeName System.Text.StringBuilder
+                $html.AppendLine("
+                <html>
+                    <head>
+                        <title>Backup Report</title>
+                        <style>
+                            table, tr, td {
+                                border: 1px solid green;
+                                border-collapse: collapse;
+                            }
+                            
+                            tr.alt td {
+                                background-color: `#171717;
+                            }
+
+                            tr.heading td {
+                                font-weight: bold;
+                                text-align: center;
+                                font-size: larger;
+                                color: white;
+                                background-color: `#333333;
+                            }
+
+                            body {
+                                background-color: black;
+                                color: `#bdbdbd;
+                                font-family: lucida consolas, monospace;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <table>
+                            <tr class=`"heading`">
+                                <td colspan=`"2`">$($computer) Backup Report</td>
+                            </tr>
+                            <tr>
+                                <td>Report</td>
+                                <td>$($cmdletName)</td>
+                            </tr>
+                            <tr>
+                                <td>Date</td>
+                                <td>$(Get-Date)</td>
+                            </tr>
+                        </table>
+                        </br>
+                        <table>
+                            <tr class=`"heading`">
+                                <td>ComputerName</td>
+                                <td>LastSuccessfulBackupTime</td>
+                                <td>LastSuccessfulBackupTargetPath</td>
+                                <td>NextBackupTime</td>
+                                <td>BareMetalRecovery</td>
+                                <td>SystemState</td>
+                                <td>VssBackupOptions</td>
+                            </tr>
+                            <tr>
+                                <td>$($result.ComputerName)</td>
+                                <td>$($result.LastSuccessfulBackupTime)</td>
+                                <td>$($result.LastSuccessfulBackupTargetPath)</td>
+                                <td>$($result.NextBackupTime)</td>
+                                <td>$($result.BareMetalRecovery)</td>
+                                <td>$($result.SystemState)</td>
+                                <td>$($result.VssBackupOptions)</td>
+                            </tr>
+                            <tr class=`"heading`">
+                                <td colspan=`"7`">
+                                        Volumes to Backup
+                                </td>
+                            </tr>") | Out-Null
+                foreach ($volume in $result.VolumesToBackup)
+                {
+                    $html.AppendLine("
+                        <tr>
+                            <td colspan=`"7`">$($volume)</td>
+                        </tr>") | Out-Null
+                }
+                $html.AppendLine("
+                    </table>
+                </body>
+                </html") | Out-Null
+                $html.ToString() | Out-File -FilePath ('{0}\{1}\{1}_BackupStatusReport_{2}.html' -f $FilePath, $computer, (Get-Date -Format 'yyyyMMdd')) -Force
+
+
+                Write-Verbose -Message ('{0} : {1} : End execution' -f $cmdletName, $computer)
             }
-            catch
-            {
-                throw
-            }
+        }
+        catch
+        {
+            throw
         }
     }
 
