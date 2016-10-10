097c5231fd51cff55e1a2210df2e590c87e80af0
Alex <george.Sparkman87@gmail.com>
2016-10-09 20:49:40 -0500

    Bing Points, Rack em up!
    Rack em and stack em! Maybe find a way to do this from Edge, not IE, to
    get even more points!

---
+++ Get-d00mBingPoints.ps1
@@ -0,0 +1,38 @@
+﻿Add-Type -AssemblyName System.Web
+
+function Get-d00mBing
+{
+    param
+    (
+        [parameter(Mandatory,
+                   ValueFromPipeline,
+                   Position = 0)]
+        [string[]]$Query
+    )
+
+    foreach ($q in $Query)
+    {
+        $ie = New-Object -ComObject InternetExplorer.Application
+        $q = [System.Web.HttpUtility]::UrlEncode($q)
+        $ie.Navigate2(('https://www.bing.com/search?q={0}' -f $q))
+        $ie.Visible = $true
+        Start-Sleep -Seconds 2
+        Get-Process iexplore | Stop-Process -Force
+    }
+}
+
+Function Get-d00mGoogleTrends
+{
+    [xml]$result = Invoke-WebRequest -Uri http://www.google.com/trends/hottrends/atom/feed?pn=p1
+    $result.rss.channel.item.title | Write-Output
+}
+
+Function Get-d00mBingPoints
+{
+    $i = 0
+    while ($i -lt 2)
+    {
+        Get-d00mGoogleTrends | Get-d00mBing
+        $i++
+    }
+}
