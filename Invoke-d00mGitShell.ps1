622cc958552ad1167d51a8339a99214de12c5cf0
Alex <george.Sparkman87@gmail.com>
2016-10-09 19:05:35 -0500

    Commited Invoke-d00mGitShell.ps1 WITH Invoke-d00mGitShell! Whahhahahaha

---
+++ Invoke-d00mGitShell.ps1
@@ -0,0 +1,10 @@
+﻿$gitPath = "$env:APPDATA\..\Local\GitHub\GitHub.appref-ms"
+
+if (Test-Path $gitPath)
+{
+    Start-Process -FilePath $gitPath -ArgumentList '--open-shell'
+}
+else
+{
+    Write-Output $false
+}
