b3d3d189968557ff3c7fa15536ebbaafbfc17c73
Alex <george.Sparkman87@gmail.com>
2016-10-10 11:01:17 -0500

    Bing Reward Points!
    Automate those searches! Now using Edge for double the fun!

---
+++ Get-d00mBingRewardPoints.ps1
@@ -0,0 +1,26 @@
+﻿function Get-d00mBingRewardPoints
+{
+    try
+    {
+        [xml]$result = Invoke-WebRequest -Uri http://www.google.com/trends/hottrends/atom/feed?pn=p1 -UseBasicParsing
+        $result.rss.channel.item.title | ForEach-Object {
+            $q = [System.Web.HttpUtility]::UrlEncode($_)
+            $url = 'https://www.bing.com/search?q={0}' -f $q
+            Start microsoft-edge:$url
+            Start-Sleep -Seconds 3
+        }
+        $result = $null
+
+        [xml]$result = Invoke-WebRequest -Uri http://www.google.com/trends/hottrends/atom/feed?pn=p2 -UseBasicParsing
+        $result.rss.channel.item.title | ForEach-Object {
+            $q = [System.Web.HttpUtility]::UrlEncode($_)
+            $url = 'https://www.bing.com/search?q={0}' -f $q
+            Start microsoft-edge:$url
+            Start-Sleep -Seconds 3
+        }
+    }
+    catch
+    {
+
+    }
+}
