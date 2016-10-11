72d882c74fdfe6ff0ff51d86378d5088fb270a98
Alex <george.Sparkman87@gmail.com>
2016-10-11 08:38:11 -0500

    Bing Reward Points
    Modified so that only uses 10 results from Google trends (second page of
    results didn't want to parse correctly into xml) and added assembly
    reference to System.Web

--- Get-d00mBingRewardPoints.ps1
+++ Get-d00mBingRewardPoints.ps1
@@ -1,5 +1,6 @@
 ﻿function Get-d00mBingRewardPoints
 {
+    Add-Type -AssemblyName System.Web | Out-Null
     try
     {
         [xml]$result = Invoke-WebRequest -Uri http://www.google.com/trends/hottrends/atom/feed?pn=p1 -UseBasicParsing
@@ -9,18 +10,9 @@
             Start microsoft-edge:$url
             Start-Sleep -Seconds 3
         }
-        $result = $null
-
-        [xml]$result = Invoke-WebRequest -Uri http://www.google.com/trends/hottrends/atom/feed?pn=p2 -UseBasicParsing
-        $result.rss.channel.item.title | ForEach-Object {
-            $q = [System.Web.HttpUtility]::UrlEncode($_)
-            $url = 'https://www.bing.com/search?q={0}' -f $q
-            Start microsoft-edge:$url
-            Start-Sleep -Seconds 3
-        }
     }
     catch
     {
-
+        throw
     }
 }
