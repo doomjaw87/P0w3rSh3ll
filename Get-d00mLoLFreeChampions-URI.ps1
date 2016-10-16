5bd95741e3edb0348e26d8a0aa3f020c42ebfb15
Alex <george.Sparkman87@gmail.com>
2016-10-16 12:50:11 -0500

    LoL Free Champs
    Get the Free to Play League of Legends Champions

---
+++ Get-d00mLoLFreeChampions-URI.ps1
@@ -0,0 +1,13 @@
+﻿Clear-Host
+
+$championRotation = New-Object -TypeName System.Collections.ArrayList
+(Invoke-WebRequest -uri http://freechampionrotation.com/ -UseBasicParsing).Images.OuterHTMl | ForEach-Object {
+    $championRotation.Add($_.Split('"')[3]) | Out-Null
+}
+
+$champs = $championRotation | Select-Object -Unique
+$champs.ForEach{
+    $uri = 'http://leagueoflegends.wikia.com/wiki/{0}' -f ($_.Replace(' ','_'))
+    $result = Invoke-WebRequest -Uri $uri -UseBasicParsing
+    $result.RawContent
+}
