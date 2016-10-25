53190775825b590c306025c14fec4d385a07f47f
Alex Sparkman <george.sparkman87@gmail.com>
2016-10-25 17:29:18 -0500

    Get-d00mDirectorySize
    PowerShell function that retrieves directory sizes

--- Get-d00mDirectorySize.ps1
+++ Get-d00mDirectorySize.ps1
@@ -1,33 +1,51 @@
 ﻿function Get-d00mDirectorySize
 {
+    [cmdletbinding()]
     param
     (
-        [parameter(Mandatory)]
-        $Path
+        [parameter(mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName,
+                   Position=0)]
+        [string[]]$DirectoryPath
     )
 
     begin
     {
-        $properties = @(
-        @{Name       = 'Directory'
-          Expression = {$_.Name}},
-        @{Name       = 'Size'
-          Expression = {(Get-ChildItem -Path $_.FullName -Recurse | Measure-Object -Property Length -Sum | Select-Object -Property $_.Size).Sum}}
-        )
+        $timer = New-Object -TypeName System.Diagnostics.Stopwatch
+        $cmdletName = $MyInvocation.MyCommand.Name
         
+        $timer.Start()
+        Write-Verbose -Message ('{0} : Begin execution {1}' -f $cmdletName, (Get-Date))
     }
 
     process
     {
-        $files = Get-ChildItem -Path $Path | Where-Object -Property Attributes -eq 'Directory'
-        foreach ($file in $files)
+        foreach ($dPath in $DirectoryPath)
         {
-            $file | select $properties
+            Write-Verbose -Message ('{0} : {1} : Begin execution' -f $cmdletName, $dPath)
+            try
+            {
+                Get-ChildItem -Path $dPath -Directory | ForEach-Object {
+                    $dirPath = $_.FullName
+                    $size = (Get-ChildItem -Path $_.FullName -Recurse -Force | Measure-Object -Property length -Sum).Sum/1mb
+                    New-Object -TypeName psobject -Property @{PathName = $dirPath
+                                                              SizeMB   = ('{0:N2}' -f ($size))} |
+                    Write-Output
+                }
+            }
+            catch
+            {
+
+            }
+
+            Write-Verbose -Message ('{0} : {1} : End execution' -f $cmdletName, $dPath)
         }
     }
 
     end
     {
-
+        $timer.Stop()
+        Write-Verbose -Message ('{0} : End execution. {1} ms' -f $cmdletName, $timer.ElapsedMilliseconds)
     }
 }
