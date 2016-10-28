25152595ad461d0b78f86ccfe678f6d428e64ffb
Alex Sparkman <george.sparkman87@gmail.com>
2016-10-28 17:03:19 -0500

    examples
    examples

---
+++ Examples/ErrorHandling.ps1
@@ -0,0 +1,16 @@
+﻿<#########################
+# Cmdlet Error Reporting #
+#########################>
+
+# These will error and flood the console with red error text
+$data1 = Get-ChildItem -Path C:\NotARealDirectory -Filter *.ps1
+$data2 = Get-Process -FileVersionInfo
+
+
+# These will still error but not flood the console with red error text
+$data1 = Get-ChildItem -Path C:\NotARealDirectory -Filter *.ps1 -ErrorAction SilentlyContinue -ErrorVariable errorList
+$data2 = Get-Process -FileVersionInfo -ErrorAction SilentlyContinue -ErrorVariable +errorList
+
+# The ErrorVariable errorList will then be available to do whatever you want with it
+$issues = $errorList.CategoryInfo | Select-Object -Property Category, TargetName
+$issues
---
+++ Examples/FileNames.ps1
@@ -0,0 +1,29 @@
+﻿<#####################
+# Illegal Characters #
+#####################>
+
+# Path to Check
+$filenameToCheck = 'testfile:?.txt'
+
+# Get invalid characters and escape them for use with RegEx
+$illegal = [Regex]::Escape(-join [System.IO.Path]::GetInvalidFileNameChars())
+$pattern = '[{0}]' -f $illegal
+
+# Find illegal characters
+$invalid = [regex]::Matches($filenameToCheck, $pattern, 'IgnoreCase').Value | 
+    Sort-Object -Unique
+
+if ($invalid)
+{
+    $characters = New-Object -TypeName System.Text.StringBuilder
+    foreach ($invalidCharacter in $invalid)
+    {
+        $characters.Append(('{0} ' -f $invalidCharacter)) |
+            Out-Null
+    }
+    'Do not use invalid characters in file names... {0}' -f $characters.ToString()
+}
+else
+{
+    'All is good in the hood.'
+}
