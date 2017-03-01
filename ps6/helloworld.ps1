81f01e6c3b456f437cf190cfd83ed75a5e00cbf8
Alex Sparkman <george.sparkman87@gmail.com>
2017-03-01 16:49:47 -0600

    stuff

--- ps6/helloworld.ps1
+++ ps6/helloworld.ps1
@@ -1,1 +1,5 @@
 write-host 'hello world!'
+$ip = Invoke-RestMethod -Uri http://checkip.amazonaws.com
+@{ComputerName = hostname
+  PublicIp     = [ipaddress]$ip.Trim()}
+New-Object -TypeName psobject -Property $props | Write-Output
