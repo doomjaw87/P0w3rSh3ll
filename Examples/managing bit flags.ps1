5244b885348437f26c5225fdc0b268c2e1a29ae7
Alex Sparkman <george.sparkman87@gmail.com>
2017-03-06 12:56:24 -0600

    Bit Flags
    the all important bit flags and enumerations

--- Examples/managing bit flags.ps1
+++ Examples/managing bit flags.ps1
@@ -56,3 +56,63 @@ $flags.HasFlag([CustomBitFlags]::Option1)
 
 $flags.HasFlag([CustomBitFlags]::Option4)
 # false
+
+
+
+<##########################
+| MANAGING BIT FLAGS PT 2 |
+###########################
+
+In the previous tip we illustrated how you can use PowerShell 5's new enumsx to easily decipher
+big flags, and even test for individual flags.
+
+If you cannot use PowerShell 5, in older PowerShell versions, you can still use this technique.
+Simply define the enum via C# code.
+
+#>
+
+# this is the decimal we want to decipher
+$rawFlags = 56823
+
+# define an enum with the friendly names for the flags
+# don't forget [Flags]
+# IMPORTANT: you cannot change your type inside a PowerShell session!
+# if you made changes to the enum, close PowerShell and open a new
+# PowerShell!
+$enum ='
+using System;
+[Flags]
+public enum BitFlags 
+{
+    None    = 0,
+    Option1 = 1,
+    Option2 = 2,
+    Option3 = 4,
+    Option4 = 8,
+    Option5 = 16,
+    Option6 = 32,
+    Option7 = 64,
+    Option8 = 128,
+    Option9 = 256,
+    Option10= 512,
+    Option11= 1024,
+    Option12= 2048,
+    Option13= 4096,
+    Option14= 8192,
+    Option15= 16384,
+    Option16= 32768,
+    Option17= 65536
+}'
+Add-Type -TypeDefinition $enum
+
+# convert the decimal to the new enum
+[BitFlags]$flags = $rawFlags
+$flags
+#Option1, Option2, Option3, Option5, Option6, Option7, Option8, Option9, Option11, Option12, Option13, Option15, Option16
+
+# test individual flags
+$flags.HasFlag([BitFlags]::Option1)
+#true
+
+$flags.HasFlag([BitFlags]::Option2)
+#true
