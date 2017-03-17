a5c892dcef3fd44f5d4b2f7f88800768affe41b5
Alex Sparkman <george.sparkman87@gmail.com>
2017-03-17 14:59:42 -0500

    powershell examples
    powershell daily tips examples

---
+++ Examples/texts with maximum length.ps1
@@ -0,0 +1,16 @@
+﻿<#####################################
+| TEXTS WITH MAXIMUM LENGTH (PART 1) |
+######################################
+
+If you want to make sure a text is of given length, here is an easy approach:
+
+#>
+
+$text = 'this is a long text'
+$maxLength = 10
+
+$text.PadRight($maxLength).Substring(0, $maxLength)
+
+
+# This code first pads the text in case it is shorter than the maximum length, and then SubString()
+# is cutting off excess text
---
+++ Examples/where-object and .where().ps1
@@ -0,0 +1,29 @@
+﻿<############################
+| WHERE-OBJECT AND .WHERE() |
+#############################
+
+Beginning in PowerShell 4, you can use the Where() and Foreach() methods in place of Where-Object
+and ForEach-Object whenever you do not want to use the streaming mechanism of the pipeline.
+
+So if you already leaded all data into a variable, the non-streaming mechanism can be faster:
+
+#>
+
+$services = Get-Service
+
+# Pipeline streaming
+$services | Where-Object {$_.Status -eq 'Running'}
+
+# Non-streaming
+$services.Where{$_.State -eq 'Running'}
+
+
+# To save on resources, the most efficient way remains the streaming pipeline mode, and not
+# using variables:
+
+Get-Service | Where-Object {$_.Status -eq 'Running'}
+
+# NOTE: Where-Object and .Where() use different array types, so their output is technically
+# speaking not identical:
+(1..10 | Where-Object {$_ -gt 0}).GetType().FullName # = System.Object[]
+((1..10).Where{$_ -gt 0}).GetType().FullName # = System.Collections.ObjectModel.Collection`1[[System.Management.Automation.PSObject, System.Management.Automation, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35]]
