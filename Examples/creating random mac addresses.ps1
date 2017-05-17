61a6b80e8070c753f8b0a73599e378771656196a
Alex Sparkman <george.sparkman87@gmail.com>
2017-05-17 12:49:50 -0500

    creating random mac addresses
    create random mac addresses

---
+++ Examples/creating random mac addresses.ps1
@@ -0,0 +1,22 @@
+﻿<################################
+| CREATING RANDOM MAC ADDRESSES |
+#################################
+
+If you just need a bunch of randomly generated MAC addresses, and you don't care much about
+whether these addresses are actually valid, then here is a one liner
+
+#>
+
+(0..5 | ForEach-Object {
+    '{0:x}{1:x}' -f (Get-Random -Minimum 0 -Maximum 15), (Get-Random -Minimum 0 -Maximum 15)
+}) -join ':'
+
+
+# Add it to a loop, and you get as many MAC addresses as you need:
+0..100 |
+    ForEach-Object {
+        (0..5 |
+            ForEach-Object {
+                '{0:x}{1:x}' -f (Get-Random -Minimum 0 -Maximum 15), (Get-Random -Minimum 0 -Maximum 15)
+            }) -join ':'
+    }
