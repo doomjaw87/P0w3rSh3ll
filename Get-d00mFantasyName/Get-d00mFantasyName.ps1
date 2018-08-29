baa709774a8063ddfb5588aa8040746c18e68042
Sparkman <Alex.Sparkman@gartner.com>
2018-08-29 15:53:48 -0500

    fantasy name generator function

---
+++ Get-d00mFantasyName/Get-d00mFantasyName.ps1
@@ -0,0 +1,18 @@
+function Get-d00mFantasyName
+{
+    param (
+        [parameter()]
+        [ValidatePattern('^[a-zA-Z]$')]
+        [string]
+        $Letter,
+
+        [parameter]
+        [int]
+        $Count
+    )
+
+    if ($Letter -ne $null)
+    {
+        
+    }
+}
